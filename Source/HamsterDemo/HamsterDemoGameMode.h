9bf89b3d88ab42c753debd4da17658868b095edd
ParkSolHwi <psh50zmfhtm@gmail.com>
2023-05-04 01:04:42 +0900

    initialize

---
+++ Source/HamsterDemo/HamsterDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HamsterDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHamsterDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHamsterDemoGameMode();
+};
+
+
+
